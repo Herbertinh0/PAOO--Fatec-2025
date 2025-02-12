2fb45002b3647fe5e4b0ee06af05e05a57ef6831
Herbert Carvalho <herbertfscarvalho@gmail.com>
2025-02-12 17:28:03 -0300

    learn(ap01-js): encerra a aula de introdução ao JavaScript - com var, let e const

---
+++ intro_js/script.js
@@ -0,0 +1,42 @@
+//*Aula 01 (12/02/2025) - Introdução ao JavaScript*//
+
+////////////////////////////////////////////////////////////////////////
+
+//*DECLARANDO VARIÁVEIS*//
+//O VAR NÃO RESPEITA O ESCOPO DE BLOCO, ELE É GLOBAL🌐
+//JÁ O LET RESPEITA O ESCOPO DE BLOCO🔒
+
+//PQ NÃO USAR O VAR?//
+// var idade = 18
+// console. log( 'Oi, ' + nome)
+// //IÇAMENTO (HOISTING)//
+// if(idade >= 18){
+//     var nome = 'Genésio'
+//     console.log ('Parabéns ' + nome + ', Você pode dirigir!')
+// }
+// console.log('Hasta la vista ' + nome + '!')  
+
+// var linguagem  = "Python"; 
+// console.log("Estou aprendendo " + linguagem + '!');
+// var linguagem = "JavaScript";
+// console.log("Estou aprendendo " + linguagem + '!');
+
+// var a = 10;
+// console.log(a); 
+// a = 20; 
+// console.log(a);        
+
+// let  a = 10;
+// console.log(a);
+// a = 20;
+// console.log(a);
+
+////////////////////////////////////////////////////////////////////////
+
+//*DECLARANDO CONSTANTES*//
+// const idade = 27
+// Se fosse em Java: String nome = 'Zé da Manga';
+// const nome =  'Zé da Manga';
+// console.log(nome)
+// nome = 'Agnaldo';
+// console.log(nome)
