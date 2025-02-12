8f58de1a77820a5bfddf6bb15f92ba00e2aa9816
Herbert Carvalho <herbertfscarvalho@gmail.com>
2025-02-12 18:16:56 -0300

    learn ap01 (1.6.1) - retomar funcões - ajustar comparação - ajustar retorno de funções

--- intro_js/script.js
+++ intro_js/script.js
@@ -1,5 +1,47 @@
 //*Aula 01 (12/02/2025) - Introdução ao JavaScript*//
 
+//*FUNÇÕES*//
+function hello() {
+    console.log('Hello, World!')
+}
+//chamar a função
+hello()
+
+function hello(nome) {
+    console.log('Hello, ' + nome + '!')
+}
+hello('Genésio')
+
+//*VETORES*//
+// v1 = []
+// console.log(v1.length)
+// v1[0] = 3.4
+// console.log(v1.length)
+// v1[1] = 2
+// console.log(v1.length)
+// v1[2] = 'Olá'
+// console.log(v1.length)
+
+// console.log(v1)
+
+// for (let i = 0; i < v1.length; i++) {
+//     console.log(v1[i])
+// }
+
+//*COMPARAÇÃO COM == e  ===//
+
+
+
+//*COERÇÃO DE TIPOS*//
+// const n1 = 2
+// const n2 = '5'
+// //coerção implícita
+// const n3 = n1 + n2
+// console.log(n3)
+// //coerção explícita
+// const n4 = n1 + Number(n2)
+// console.log(n4)
+
 ////////////////////////////////////////////////////////////////////////
 
 //*DECLARANDO VARIÁVEIS*//
